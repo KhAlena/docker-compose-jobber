3c5f0142286cfc49121b8d63d6b54904e1f73820
Khalena <as-alex93@mail.ru>
2022-04-02 22:45:28 +0300

    Initial commit

---
+++ docker/jobber/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "${1#-}" != "$1" ]; then
+  set -- docker "$@"
+fi
+
+if docker help "$1" > /dev/null 2>&1; then
+  set -- docker "$@"
+fi
+
+if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
+  export DOCKER_HOST='tcp://docker:2375'
+fi
+
+#exec "$@"
+/usr/libexec/jobberrunner -u /var/jobber/0/cmd.sock /jobs/.jobber
+
